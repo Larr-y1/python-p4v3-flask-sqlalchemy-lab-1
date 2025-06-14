1aff6d9dd384c0589575f7a912676cb8fe38b148
Larry <larryotieno51@gmail.com>
2025-06-14 07:32:00 +0300

    completed flask sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,42 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route("/earthquakes/<int:id>")
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    
+    if earthquake:
+        response_body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        return make_response(jsonify(response_body), 200)
+    else:
+        error_msg = {"message": f"Earthquake {id} not found."}
+        return make_response(jsonify(error_msg), 404)
 
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    result = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        for quake in quakes
+    ]
 
+    response_body = {
+        "count": len(result),
+        "quakes": result
+    }
+
+    return make_response(jsonify(response_body), 200)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
